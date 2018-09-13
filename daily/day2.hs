3b163f976214e869debed61193584b3f7704fbc8
Daniel <djustice@wificidr.net>
2018-09-12 21:20:46 -0500

    Not sure where the 2 came from

--- daily/day2.hs
+++ daily/day2.hs
@@ -1,6 +1,6 @@
 myList = [3, 2, 1]
 
 prodErizer :: (Integral a) => [a] -> [a]
-prodErizer xs = [prodParts (partSplit n xs) | n <- [2..length(xs)]]
+prodErizer xs = [prodParts (partSplit n xs) | n <- [1..length(xs)]]
   where partSplit = (\n -> splitAt n)
         prodParts = (\x -> product (init (fst x) ++ snd x))
